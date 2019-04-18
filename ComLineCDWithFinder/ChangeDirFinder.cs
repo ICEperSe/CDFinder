a267f0621634e0e4365ee04f43c31c5f70084c82
ICEperSe <egorstrelov@gmail.com>
2019-04-19 00:47:00 +0300

    FluentAssertions were added, first two tests

---
+++ ComLineCDWithFinder/ChangeDirFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComLineCDWithFinder
+{
+    public class ChangeDirFinder
+    {
+        public DirectoryInfo CurrentDirectory;
+
+        public ChangeDirFinder(string curDir)
+        {
+            CurrentDirectory = new DirectoryInfo(curDir);
+        }
+
+        public string GetPathTo(string targetDir)
+        {
+            if(targetDir == string.Empty)
+                throw new ArgumentException(nameof(targetDir));
+            //if(targetDir == CurrentDirectory.Name)
+            return null;
+        }
+    }
+}
---
+++ ComLineCDWithFinder/ChangeDirFinder_Should.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace ComLineCDWithFinder
+{
+    [TestFixture]
+    public class ChangeDirFinder_Should
+    {
+        private ChangeDirFinder cdFinder;
+        private DirectoryInfo curDirectory;
+        [SetUp]
+        public void SetUp()
+        {
+            curDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            cdFinder = new ChangeDirFinder(curDirectory.FullName);
+        }
+
+        [TestCase("")]
+        public void Throw_OnEmptyInput(string path)
+        {
+            Assert.Throws<ArgumentException>(()=>cdFinder.GetPathTo(path));
+        }
+
+        [Test]
+        public void ReturnCurDir_OnCurDirInput()
+        {
+            cdFinder.GetPathTo(curDirectory.Name).Should().Be(curDirectory.Name);
+        }
+    }
+}
