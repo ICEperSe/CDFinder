6e01559a891655b30c159081e7a4ec95b316b01a
ICEperSe <egorstrelov@gmail.com>
2019-04-30 22:47:43 +0300

    WinCMDShell start refactoring

--- ComLineCDWithFinder/Properties/AssemblyInfo.cs
+++ ComLineCDWithFinder/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("cdFinder")]
+[assembly: AssemblyProduct("cdf")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- ComLineCDWithFinder/WinCMDShell.cs
+++ ComLineCDWithFinder/WinCMDShell.cs
@@ -12,40 +12,84 @@ namespace ComLineCDWithFinder
 {
     static class WinCMDShell
     {
+        private static bool IsConsoleFree = false;
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool AttachConsole(uint dwProcessId);
+        private static extern bool AttachConsole(uint dwProcessId);
 
         [DllImport("kernel32")]
-        static extern bool FreeConsole();  
+        private static extern bool FreeConsole();  
     
         [DllImport("user32.dll")]
-        static extern bool SetForegroundWindow(IntPtr hWnd);
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
 
         public static void Run(string target)
         {
+            CheckConsoleState();
             var curDir = Environment.CurrentDirectory;
             var targetDirs = PathFinder.GetPathTo(curDir, target);
-            if (targetDirs.Length == 0) Console.WriteLine("There is no such directory");
+            if (targetDirs.Length == 0)
+            {
+                Console.WriteLine("There is no such directory");
+                return;
+            }
             if (targetDirs.Length == 1)
             {
-                Process[] procs = Process.GetProcessesByName("cmd");
-                if (procs.Length > 0)
-                {
-                    int pId = procs[0].Id;
-                    FreeConsole();
-                    bool ok = AttachConsole((uint) pId);
-                    if (!ok)
-                    {
-                        int error = Marshal.GetLastWin32Error();
-                        Console.WriteLine(error);
-                    }
-                    else
-                    {
-                        SetForegroundWindow(Process.GetCurrentProcess().MainWindowHandle);
-                        SendKeys.SendWait("cd " + targetDirs[0]);
-                    }
-                }
+                WriteResult(targetDirs[0]);
+            }
+            else
+            {
+                if(SelectSingleOption(targetDirs, out var path))
+                    WriteResult(path);
+            }
+        }
+
+        private static bool SelectSingleOption(IReadOnlyList<string> options, out string path)
+        {
+            if (options == null) throw new ArgumentNullException(nameof(options));
+            var i = 0;
+            foreach (var dir in options)
+            {
+                Console.WriteLine($"{++i}. {dir}");
+            }
+            Console.Write("Enter number: ");
+            if (int.TryParse(Console.ReadLine(), out var numb) 
+                && numb <= options.Count && numb > 0)
+            {
+                path = options[numb - 1];
+                return true;
+            }
+
+            path = null;
+            return false;
+        }
+
+        private static void WriteResult(string resultPath)
+        {
+            CheckConsoleState();
+            var process = Process.GetProcessesByName("cmd");
+            if (process.Length <= 0) return;
+            var pId = process[0].Id;
+            FreeConsole();
+            if (!AttachConsole((uint) pId))
+            {
+                Console.WriteLine(Marshal.GetLastWin32Error());
+                return;
+            }
+            else
+            {
+                SetForegroundWindow(Process.GetCurrentProcess().MainWindowHandle);
+                SendKeys.SendWait("cd " + resultPath);
+            }
+
+            IsConsoleFree = true;
+        }
+
+        private static void CheckConsoleState()
+        {
+            if (IsConsoleFree)
+            {
+                throw new InvalidOperationException();
             }
         }
     }
